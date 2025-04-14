a659077021c7fec35327bdc5b18367b22b844108
Suryakl64 <suryakl7416@gmail.com>
2025-04-14 23:56:28 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatInterface from "./components/ChatInterface";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ marginLeft: 10 }}>Math Solver AI</h1>
+      <ChatInterface />
+    </div>
+  );
+}
+
+export default App;
